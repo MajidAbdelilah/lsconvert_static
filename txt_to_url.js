258de7157911269b5967e9e9572ec41e75109f70
MajidAbdelilah <abdelilahmajid9@gmail.com>
2022-11-09 21:53:37 +0100

    front end

--- txt_to_url.js
+++ txt_to_url.js
@@ -11,12 +11,28 @@ if(!localStorage.date_number || !localStorage.month_number){
     localStorage.date_number =  new Date().getDate();
     localStorage.month_number = new Date().getMonth();
 }
+
+let block_sending = false;
+
 input.addEventListener("change", ()=>{
     inputFile = document.querySelector("input[type=file]").files[0];
+
+    let name_len = inputFile.name.length;
+    console.log(name_len);
     
-    fileName.innerText = inputFile.name;
-    document.querySelector("#file_size_warnning").textContent = ""
+    if(inputFile.name.charAt(name_len-1) == "t" &&
+       inputFile.name.charAt(name_len-2) == "x" &&
+       inputFile.name.charAt(name_len-3) == "t" &&
+       inputFile.name.charAt(name_len-4) == "."){
+	fileName.innerText = inputFile.name;
+	document.querySelector("#file_size_warnning").textContent = "";
+	block_sending = false;
+    }else{
+	document.querySelector("#file_size_warnning").textContent = "الملف يجب ان يكون بصيغة .txt";
+	block_sending = true;
+    }
     
+    console.log(block_sending)
 })
 
 document.querySelector("#form").addEventListener("submit", function(e){
@@ -24,10 +40,15 @@ document.querySelector("#form").addEventListener("submit", function(e){
     let date = new Date().getDate();
     console.log(date);
     if(!localStorage.number_of_uses) localStorage.number_of_uses = 0;
-    else if(fileName.innerText != "" && localStorage.number_of_uses<=5 && inputFile.size <= 500000)
+    else if(fileName.innerText != "" && localStorage.number_of_uses<=5 && inputFile.size <= 500000 && !block_sending)
 	localStorage.number_of_uses++;
     console.log(localStorage.number_of_uses);
     //e.preventDefault();
+
+    if(block_sending){
+	e.preventDefault();
+
+    }
     
     if(localStorage.number_of_uses>5 && fileName.innerText != ""){
 	e.preventDefault();
@@ -43,5 +64,7 @@ document.querySelector("#form").addEventListener("submit", function(e){
 	e.preventDefault();
 	document.querySelector("#file_size_warnning").textContent = "حجم الملف اكبر من الحجم المسموح به"
     }
+
+    //console.log()
     
 });
